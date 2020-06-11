01fbfd8bc21933858aede271a9e9247d9fc68b19
MetoA <metoandov@gmail.com>
2020-06-11 17:27:43 +0200

    Updates

--- src/components/SvgWeather.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const SvgWeather = ({ data, weatherDesc }) => {
-  return (
-    <object type="image/svg+xml" data={data} className="svg">
-      {weatherDesc}
-    </object>
-  );
-};
